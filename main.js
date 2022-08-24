bca4d654b528a6cf33ecfd1bb5f0758cc0be9c1f
taenyx <jorge.chavez4507@gmail.com>
2022-08-24 00:29:56 -0500

    Se realizó un comentario

--- main.js
+++ main.js
@@ -3,4 +3,6 @@ var km=prompt("ingresar kilómetros")
 var gas=prompt("ingresar litros de gasolina")
 var resultado=km/gas
 console.log(resultado)
+//Se terminó el commit, el push y el pull
+console.log("fin")
 }
